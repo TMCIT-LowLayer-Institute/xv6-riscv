cd02278e64edc93456618d06b29ed03a0a141435
Unixtech-06 <ryosuke_666@icloud.com>
2024-03-10 01:01:52 +0900

    Initialized all local variables in user/grep.c.

--- user/grep.c
+++ user/grep.c
@@ -10,8 +10,8 @@ int match(char*, char*);
 void
 grep(char *pattern, int fd)
 {
-  int n, m;
-  char *p, *q;
+  int n = 0, m = 0;
+  char *p = nullptr, *q = nullptr;
 
   m = 0;
   while((n = read(fd, buf+m, sizeof(buf)-m-1)) > 0){
@@ -36,8 +36,8 @@ grep(char *pattern, int fd)
 int
 main(int argc, char *argv[])
 {
-  int fd, i;
-  char *pattern;
+  int fd = 0, i = 0;
+  char *pattern = nullptr;
 
   if(argc <= 1){
     fprintf(2, "usage: grep pattern [file ...]\n");
